ac273529e596e1fa210322177f884f3076eebfc7
Grunky Bag o' Bolts <88250471+grunkyb@users.noreply.github.com>
2023-10-10 13:10:35 +0100

    Added deflate compress

--- micropython/board/RPI_PICO_W/mpconfigboard.h
+++ micropython/board/RPI_PICO_W/mpconfigboard.h
@@ -14,6 +14,9 @@
 #define CYW43_GPIO (1)
 #define CYW43_SPI_PIO (1)
 
+// Enable deflate's compress build option
+#define MICROPY_PY_DEFLATE_COMPRESS (1)
+
 // For debugging mbedtls - also set
 // Debug level (0-4) 1=warning, 2=info, 3=debug, 4=verbose
 // #define MODUSSL_MBEDTLS_DEBUG_LEVEL 1
